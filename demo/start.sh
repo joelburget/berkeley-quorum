5468381330f4478d3418dc1c761764baf5cb1503
Joel Burget <joelburget@gmail.com>
2017-11-06 15:14:39 -0500

    Smoother start script.

--- demo/start.sh
+++ demo/start.sh
@@ -6,35 +6,22 @@ GLOBAL_ARGS="--emitcheckpoints --nodiscover --raft --rpc --rpcaddr 0.0.0.0 --rpc
 echo "[*] Starting Constellation nodes"
 nohup constellation-node tm1.conf 2>> qdata/logs/constellation1.log &
 sleep 1
-nohup constellation-node tm2.conf 2>> qdata/logs/constellation2.log &
-nohup constellation-node tm3.conf 2>> qdata/logs/constellation3.log &
-nohup constellation-node tm4.conf 2>> qdata/logs/constellation4.log &
-nohup constellation-node tm5.conf 2>> qdata/logs/constellation5.log &
-nohup constellation-node tm6.conf 2>> qdata/logs/constellation6.log &
-nohup constellation-node tm7.conf 2>> qdata/logs/constellation7.log &
+
+for i in `seq 2 7`;
+do
+  nohup constellation-node tm$i.conf 2>> qdata/logs/constellation$i.log &
+done
 
 sleep 1
 
 echo "[*] Starting node 1"
-PRIVATE_CONFIG=tm1.conf nohup geth --datadir qdata/dd1 $GLOBAL_ARGS --raftport 50401 --rpcport 22000 --port 21000 --unlock 0 --password passwords.txt 2>>qdata/logs/1.log &
+PRIVATE_CONFIG=tm1.conf nohup geth --verbosity 6 --datadir qdata/dd1 $GLOBAL_ARGS --raftport 50401 --rpcport 22001 --port 21001 --unlock 0 --password passwords.txt 2>>qdata/logs/1.log &
 
-echo "[*] Starting node 2"
-PRIVATE_CONFIG=tm2.conf nohup geth --datadir qdata/dd2 $GLOBAL_ARGS --raftport 50402 --rpcport 22001 --port 21001 2>>qdata/logs/2.log &
-
-echo "[*] Starting node 3"
-PRIVATE_CONFIG=tm3.conf nohup geth --datadir qdata/dd3 $GLOBAL_ARGS --raftport 50403 --rpcport 22002 --port 21002 2>>qdata/logs/3.log &
-
-echo "[*] Starting node 4"
-PRIVATE_CONFIG=tm4.conf nohup geth --datadir qdata/dd4 $GLOBAL_ARGS --raftport 50404 --rpcport 22003 --port 21003 2>>qdata/logs/4.log &
-
-echo "[*] Starting node 5"
-PRIVATE_CONFIG=tm5.conf nohup geth --datadir qdata/dd5 $GLOBAL_ARGS --raftport 50405 --rpcport 22004 --port 21004 2>>qdata/logs/5.log &
-
-echo "[*] Starting node 6"
-PRIVATE_CONFIG=tm6.conf nohup geth --datadir qdata/dd6 $GLOBAL_ARGS --raftport 50406 --rpcport 22005 --port 21005 2>>qdata/logs/6.log &
-
-echo "[*] Starting node 7"
-PRIVATE_CONFIG=tm7.conf nohup geth --datadir qdata/dd7 $GLOBAL_ARGS --raftport 50407 --rpcport 22006 --port 21006 2>>qdata/logs/7.log &
+for i in `seq 2 7`;
+do
+  echo "[*] Starting node $i"
+  PRIVATE_CONFIG=tm$i.conf nohup geth --datadir qdata/dd$i $GLOBAL_ARGS --raftport 5040$i --rpcport 2200$i --port 2100$i 2>>qdata/logs/$i.log &
+done
 
 echo "[*] Waiting for nodes to start"
 sleep 5
